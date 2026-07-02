7c3188e3ad3b3d6aa3aff32e88675b2972ec7b3c
lisboamotorsanuncio <lisboamotorsanuncio@gmail.com>
2026-07-02 14:00:33 -0300

    Update ofertas.js

--- dados/ofertas.js
+++ dados/ofertas.js
@@ -2,18 +2,18 @@
 window.OFERTAS = [
     {
         marca:"HYUNDAI",
-        modelo:"CRETA",
-        versao:"ACTION 2023",
-        imagem:"hyundai-creta-action.jpg",
-        specs:["1.6 FLEX","AUTOMÁTICO","ÚNICO DONO"],
-        parcela:"R$ 1.599,00"
+        modelo:"HB20",
+        versao:"SENSE 2023/2024",
+        imagem:"hyundaihb20.jpeg",
+        specs:["1.0 FLEX","AUTOMÁTICO","ÚNICO DONO"],
+        parcela:"R$ 00,00"
     },
     {
-        marca:"TOYOTA",
-        modelo:"COROLLA",
-        versao:"ACTION 2023",
-        imagem:"toyota_corolla_2021_1_25092020_35102_1280_960.jpg",
-        specs:["2.0 FLEX","AUTOMÁTICO","COMPLETO"],
-        parcela:"R$ 1.599,00"
+        marca:"JEEP",
+        modelo:"COMPASS",
+        versao:"LIMITED 2019/2020",
+        imagem:"COMPASS2.jpeg",
+        specs:["2.0 DIESEL","AUTOMÁTICO","COMPLETO"],
+        parcela:"R$ 00,00"
     }
 ];
