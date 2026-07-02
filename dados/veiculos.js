2f0d4b2232a9355da9c8c251696cb1a8db41a16c
lisboamotorsanuncio <lisboamotorsanuncio@gmail.com>
2026-07-02 14:01:21 -0300

    Update veiculos.js

--- dados/veiculos.js
+++ dados/veiculos.js
@@ -3,28 +3,148 @@ window.VEICULOS = [
     {
         marca:"JEEP",
         modelo:"COMPASS",
-        versao:"LIMITED 2022",
-        imagem:"Jeep-Compass-2022-Limited-3.jpeg",
-        specs:["2.0 TURBO DIESEL","4X4","AUTOMÁTICO","BAIXA QUILOMETRAGEM"],
+        versao:"LIMITED 2019/2020",
+        imagem:"compass.jpeg",
+        specs:["2.0 DIESEL","4X4","AUTOMÁTICO","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+
+    {
+        marca:"JEEP",
+        modelo:"COMPASS",
+        versao:"LIMITED 2019/2020",
+        imagem:"compass1.jpeg",
+        specs:["2.0 DIESEL","4X4","AUTOMÁTICO","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+
+      {
+        marca:"JEEP",
+        modelo:"COMPASS",
+        versao:"LIMITED 2019/2020",
+        imagem:"compass2.jpeg",
+        specs:["2.0 DIESEL","4X4","AUTOMÁTICO","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+  {
+        marca:"JEEP",
+        modelo:"COMPASS",
+        versao:"LIMITED 2019/2020",
+        imagem:"compass4.jpeg",
+        specs:["2.0 DIESEL","4X4","AUTOMÁTICO","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+    {
+        marca:"JEEP",
+        modelo:"COMPASS",
+        versao:"LIMITED 2019/2020",
+        imagem:"compass7.jpeg",
+        specs:["2.0 DIESEL","4X4","AUTOMÁTICO","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+
+    {
+        marca:"HYUNDAI",
+        modelo:"HB20",
+        versao:"SENSE 2023/2024",
+        imagem:"hyundaihb20.jpeg",
+        specs:["1.0 FLEX","MANUAL","ENTRADA FACILITADA",
+        "FINACIAMENTO EM ATE 48X","BAIXA KM"],
         de:"R$ 00.000,00",
         por:"R$ 00.000,00"
     },
     {
         marca:"HYUNDAI",
-        modelo:"CRETA",
-        versao:"ACTION 2023",
-        imagem:"hyundai-creta-action.jpg",
-        specs:["1.6 FLEX","AUTOMÁTICO","ÚNICO DONO","IPVA PAGO"],
+        modelo:"HB20",
+        versao:"SENSE 2023/2024",
+        imagem:"hyundaihb201.jpeg",
+        specs:["1.0 FLEX","MANUAL","ENTRADA FACILITADA",
+        "FINACIAMENTO EM ATE 48X","BAIXA KM"],
         de:"R$ 00.000,00",
         por:"R$ 00.000,00"
     },
     {
-        marca:"TOYOTA",
-        modelo:"COROLLA",
-        versao:"XEI 2021",
-        imagem:"toyota_corolla_2021_1_25092020_35102_1280_960.jpg",
-        specs:["2.0 FLEX","AUTOMÁTICO","COMPLETO","BAIXA KM"],
+        marca:"HYUNDAI",
+        modelo:"HB20",
+        versao:"SENSE 2023/2024",
+        imagem:"hyundaihb202.jpeg",
+        specs:["1.0 FLEX","MANUAL","ENTRADA FACILITADA",
+        "FINACIAMENTO EM ATE 48X","BAIXA KM"],
         de:"R$ 00.000,00",
         por:"R$ 00.000,00"
-    }
+    },
+    {
+        marca:"HYUNDAI",
+        modelo:"HB20",
+        versao:"SENSE 2023/2024",
+        imagem:"hyundaihb208.jpeg",
+        specs:["1.0 FLEX","MANUAL","ENTRADA FACILITADA",
+        "FINACIAMENTO EM ATE 48X","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+    {
+        marca:"HYUNDAI",
+        modelo:"HB20",
+        versao:"SENSE 2023/2024",
+        imagem:"hyundaihb205.jpeg",
+        specs:["1.0 FLEX","MANUAL","ENTRADA FACILITADA",
+        "FINACIAMENTO EM ATE 48X","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+    {
+        marca:"CHEVROLET",
+        modelo:"SONIC",
+        versao:"LT 2013/2013",
+        imagem:"sonic1.jpeg",
+        specs:["1.6 FLEX","AUTOMÁTICO","COMPLETO","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+
+     {
+        marca:"CHEVROLET",
+        modelo:"SONIC",
+        versao:"LT 2013/2013",
+        imagem:"sonic2.jpeg",
+        specs:["1.6 FLEX","AUTOMÁTICO","COMPLETO","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+
+     {
+        marca:"CHEVROLET",
+        modelo:"SONIC",
+        versao:"LT 2013/2013",
+        imagem:"sonic3.jpeg",
+        specs:["1.6 FLEX","AUTOMÁTICO","COMPLETO","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+
+     {
+        marca:"CHEVROLET",
+        modelo:"SONIC",
+        versao:"LT 2013/2013",
+        imagem:"sonic7.jpeg",
+        specs:["1.6 FLEX","AUTOMÁTICO","COMPLETO","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
+
+     {
+        marca:"CHEVROLET",
+        modelo:"SONIC",
+        versao:"LT 2013/2013",
+        imagem:"sonic8.jpeg",
+        specs:["1.6 FLEX","AUTOMÁTICO","COMPLETO","BAIXA KM"],
+        de:"R$ 00.000,00",
+        por:"R$ 00.000,00"
+    },
 ];
